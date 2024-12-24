f46621d3ffa229617b3f637f1f2fb06692de8ac5
phantomfills <phantomfills@outlook.com>
2024-12-24 19:03:25 +0000

    🔨improve cell ui
    - add "4" number to each counter with random rotations

--- src/client/components/cell.tsx
+++ src/client/components/cell.tsx
@@ -1,4 +1,4 @@
-import React from "@rbxts/react";
+import React, { useEffect, useState } from "@rbxts/react";
 import { Cell } from "shared/game/constants";
 
 interface CellProps {
@@ -8,6 +8,12 @@ interface CellProps {
 }
 
 export function Cell({ columnIndex, rowIndex, cell }: CellProps) {
+	const [rotation, setRotation] = useState(0);
+
+	useEffect(() => {
+		setRotation(math.random(-15, 15));
+	}, []);
+
 	return (
 		<frame
 			Size={new UDim2(0, 50, 0, 50)}
@@ -16,7 +22,7 @@ export function Cell({ columnIndex, rowIndex, cell }: CellProps) {
 		>
 			<frame Size={new UDim2(0, 40, 0, 40)} Position={new UDim2(0, 5, 0, 5)} BackgroundTransparency={1}>
 				<uicorner CornerRadius={new UDim(0.5, 0.5)} />
-				<uistroke LineJoinMode={"Miter"} Thickness={10} Color={Color3.fromRGB(171, 179, 255)} />
+				<uistroke LineJoinMode={Enum.LineJoinMode.Miter} Thickness={10} Color={Color3.fromRGB(171, 179, 255)} />
 			</frame>
 			{cell !== false && (
 				<frame
@@ -26,6 +32,18 @@ export function Cell({ columnIndex, rowIndex, cell }: CellProps) {
 						cell === "PLAYER_1" ? Color3.fromRGB(153, 255, 153) : Color3.fromRGB(255, 255, 224)
 					}
 				>
+					<textlabel
+						Text="4"
+						TextColor3={Color3.fromRGB(255, 255, 255)}
+						Size={new UDim2(0.8, 0, 0.8, 0)}
+						Position={new UDim2(0.1, 0, 0.1, 0)}
+						BackgroundTransparency={1}
+						Rotation={rotation}
+						Font={Enum.Font.GothamBold}
+						TextScaled
+					>
+						<uistroke Thickness={2} Color={Color3.fromRGB(0, 0, 0)} />
+					</textlabel>
 					<uicorner CornerRadius={new UDim(0.5, 0.5)} />
 				</frame>
 			)}
